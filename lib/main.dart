03d56997606ce5b83c3d657ebd9003e894c1883d
Eureka <eurekaripfumelo@gmail.com>
2025-12-10 20:59:53 +0200

    Menu page modifications

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home:Scaffold(
           backgroundColor: const Color.fromARGB(255, 232, 183, 47),
-          body:Center(
+          body:
+          Stack(
+            children:[
+              Positioned.fill(
+                child:Image.asset('assets/menu.png',fit:BoxFit.cover,),
+              ),
+            
+          
+          Center(
             child:Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('BOOKING.EU' ,
-                  textAlign:TextAlign.center,
-                  style:TextStyle(fontWeight:FontWeight.bold,fontSize:40)
-                ),
+              
                 SizedBox(
                   width:200,
                   child:ElevatedButton(
@@ -37,7 +42,7 @@ class MyApp extends StatelessWidget {
                   child:ElevatedButton(
                     onPressed:() {},
                     style:ElevatedButton.styleFrom(
-                    backgroundColor: Colors.teal,
+                    backgroundColor: const Color.fromARGB(255, 97, 42, 224),
                     shape:RoundedRectangleBorder(
                       borderRadius:BorderRadius.circular(10),
                     ),
@@ -46,12 +51,23 @@ class MyApp extends StatelessWidget {
 
                   )
                 ),
+                SizedBox(
+                  child:Row(
+                    children :[
+
+
+                    ],
+
+                  )
+                )
                
               ],
           ),
           ),
+          ],
         
           ),
+    )
     );
       
       
